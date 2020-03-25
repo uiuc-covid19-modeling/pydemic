b0ded5ff5419632401e7b24a4231ceb08e10c3d5
zachjweiner <zachjweiner@gmail.com>
2020-03-25 13:18:17 -0500

    minor formatting changes

--- python_pydemic.py
+++ python_pydemic.py
@@ -11,36 +11,8 @@ from pydemic import PopulationModel, AgeDistribution, SeverityModel
 URL = "http://localhost:8081"
 
 if __name__ == "__main__":
-    ## example parameter sets here
-    population = {
-    # total demographic information and labels
-    "country": "Switzerland",
-    "cases": "Switzerland",
-    "populationServed": 8600000,
-    # reported medical facilities
-    "hospitalBeds": 30799,
-    "ICUBeds": 1400,
-    # estimated infectivity model parameters
-    "suspectedCasesToday": 1148,
-    "importsPerDay": 4.0,
-    # granular population statistics
-    "populationsByDecade": [
-        4994996,
-        5733447,
-        6103437,
-        6998434,
-        9022004,
-        9567192,
-        7484860,
-        6028907,
-        4528548
-    ]
-    }
-
-    simulation = {
-    "start": [ 2020, 3, 1, 0, 0, 0 ],
-    "end": [ 2020, 9, 1, 0, 0, 0 ]
-    }
+    start_time = (2020, 3, 1, 0, 0, 0)
+    end_time = (2020, 9, 1, 0, 0, 0)
 
     epidemiology = {
         "r0": 3.7,
@@ -81,22 +53,22 @@ if __name__ == "__main__":
     )
 
 
-    ## automatically load population from json data
+    # load population from json data
+    from pydemic.load import get_country_population_model, get_age_distribution_model
+
     POPULATION_NAME = "USA-Illinois"
     AGE_DATA_NAME = "United States of America"
-    #   import pydemic.population_models as pm
-    #   age_data = pm._age_data[AGE_DATA_NAME]
-    #   population = [ x for x in pm._populations if x['name']==POPULATION_NAME ][0]['data']
-    #   population['populationsByDecade'] = [ age_data[key] for key in age_data.keys() ]
-
-    # example of kludge on how to load population data
-    from pydemic.load import get_country_population_model, get_age_distribution_model
 
     population = get_country_population_model(POPULATION_NAME)
     agedistribution = get_age_distribution_model(AGE_DATA_NAME)
 
-    ## generate and POST request to javascript api
-    body = { "simulation":simulation, "population":population, "containment":containment, "epidemiology":epidemiology, "agedistribution":agedistribution }
+    body = {
+        "simulation": simulation,
+        "population": population,
+        "containment": containment,
+        "epidemiology": epidemiology,
+        "agedistribution": agedistribution
+    }
     #data = pydemic.run(body)
 
     exit()
@@ -138,7 +110,6 @@ if __name__ == "__main__":
     ax1.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
     #ax1.set_xlim(dates[0],dates[-1])
     fig.autofmt_xdate()
-
 
     # formatting hints
     ax1.legend()
