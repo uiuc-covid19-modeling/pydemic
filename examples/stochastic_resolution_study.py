956c8b7e237f54f3d360a02f2c5f2ebe7fe69b0c
George N. Wong <gnwong2@illinois.edu>
2020-03-29 13:10:39 -0500

    modularize stochastic resolution study script

--- examples/stochastic_resolution_study.py
+++ examples/stochastic_resolution_study.py
@@ -7,7 +7,6 @@ import numpy as np
 import h5py
 import os
 
-
 def set_numpy_threads(nthreads=1):
     # see also https://codereview.stackexchange.com/questions/206736/better-way-to-set-number-of-threads-used-by-numpy  # noqa
     import os
@@ -31,7 +30,7 @@ def set_numpy_threads(nthreads=1):
     os.environ["VECLIB_MAXIMUM_THREADS"] = str(nthreads)
     os.environ["NUMEXPR_NUM_THREADS"] = str(nthreads)
 
-def generate_stochastic_data(n_sims, method, dt=0.005):
+def generate_stochastic_data(n_sims, method, datafile_name, dt=0.005):
     from pydemic.models import SEIRModelSimulation
     t_span = [0., 10.]
     y0 = {
@@ -40,56 +39,63 @@ def generate_stochastic_data(n_sims, method, dt=0.005):
         'infectious': 14.,
         'removed': 0 
     }
+    simulation = SEIRModelSimulation()
     stochastic_results = []
-    n_sims = 100
+    hfp = h5py.File(datafile_name, 'w')
+    hfp['n_sims'] = n_sims
     for i in range(n_sims):
         print(" - running stochastic ({0:s}) sample {1:d} of {2:d}".format(method, i+1, n_sims))
-        sresult = simulation(t_span, y0, lambda x:x, stochastic_method=stochastic_method, dt=timestep) 
-        stochastic_results.append(sresult)
-    pass
-
+        sresult = simulation(t_span, y0, stochastic_method=method, dt=dt)
+        packed_data = np.zeros((5, sresult.t.shape[0]))
+        packed_data[0,:] = sresult.t
+        packed_data[1,:] = sresult.y['susceptible']
+        packed_data[2,:] = sresult.y['exposed']
+        packed_data[3,:] = sresult.y['infectious']
+        packed_data[4,:] = sresult.y['removed']
+        hfp["{0:d}".format(i)] = packed_data
+    hfp.close()
 
 def load_tauleap(fname, keys):
+    if not os.path.exists(fname):
+        generate_stochastic_data(100, 'tau_leap', fname)
     data = {}
     for key in keys:
         data[key] = []
     hfp = h5py.File(fname,'r')
-    for i in range(100):
-        for key in keys:
-            fkey = "{0:d}_{1:s}".format(i, key)
-            data[key].append(np.array(hfp[fkey]))
+    n_sims = hfp['n_sims'][()]
+    for i in range(n_sims):
+        fkey = "{0:d}".format(i)
+        run_data = np.array(hfp[fkey])
+        for c in range(5):
+            data[keys[c]].append(run_data[c,:])
     hfp.close()
     return data
 
 def load_direct(fname, keys, times):
+    if not os.path.exists(fname):
+        generate_stochastic_data(100, 'direct', fname)
     data = {}
     for key in keys:
         data[key] = []
     hfp = h5py.File(fname,'r')
-    for i in range(100):
-        temp_data = {}
-        for key in keys:
-            fkey = "{0:d}_{1:s}".format(i, key)
-            temp_data[key] = np.array(hfp[fkey])
-            if key != 't':
-                f = interp1d(temp_data['t'], temp_data[key], bounds_error=False, fill_value=temp_data[key][-1])
-                data[key].append(f(times))
+    n_sims = hfp['n_sims'][()]
+    for i in range(n_sims):
+        fkey = "{0:d}".format(i)
+        temp_data = np.array(hfp[fkey])
+        for c in range(1,5):
+            key = keys[c]
+            f = interp1d(temp_data[0,:], temp_data[c,:], bounds_error=False, fill_value=temp_data[c,:][-1])
+            data[key].append(f(times))
     hfp.close()
     return data
 
 if __name__ == "__main__":
 
-
-    generate_stochastic_data(1000, 'direct')
-
-
-    exit()
-
-    # load data from tauleap and gillespie direct methods
-    keys = ["t", "susceptible", "exposed", "infectious", "removed"]
-    tauleap_data = load_tauleap("DATA_TAU_LEAP.h5", keys)
+    # load (or generate) data from tauleap and gillespie direct methods
+    keys = ['t', 'susceptible', 'exposed', 'infectious', 'removed']
+    tauleap_data = load_tauleap("data/stochastic_runs_tauleap.h5", keys)
     times = tauleap_data['t'][0]
-    direct_data = load_direct("DATA_DIRECT.h5", keys, times)
+    direct_data = load_direct("data/stochastic_runs_direct.h5", keys, times)
 
     # translate to quantiles
     quantiles = [ 0.0455, 0.3173, 0.5, 0.6827, 0.9545 ]
@@ -149,5 +155,5 @@ if __name__ == "__main__":
     ax3.set_ylabel('relative error')
     ax3.set_xlabel('time')
     plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-    plt.savefig('stochastic_comparison.png')
+    plt.savefig('imgs/stochastic_resolution_study.png')
 
