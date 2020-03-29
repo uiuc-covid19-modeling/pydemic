8b88d93c0f0450e84e38fa7b1338d5a8482b146e
George N. Wong <gnwong2@illinois.edu>
2020-03-28 22:55:50 -0500

    make code more readable

--- test_stochastic.py
+++ test_stochastic.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     n_sims = 100
     for i in range(n_sims):
         print(" - running stochastic sample (with tau leap) {0:d} of {1:d}".format(i+1,n_sims))
-        sresult = simulation(t_span, y0, lambda x:x, stochastic_method='tau_leap', dt=0.001) # 'tau_leap' versus 'direct'
+        sresult = simulation(t_span, y0, lambda x:x, stochastic_method='tau_leap', dt=0.01) # 'tau_leap' versus 'direct'
         stochastic_results.append(sresult)
 
     
@@ -70,90 +70,3 @@ if __name__ == "__main__":
     ohfp.close()
 
 
-
-
-    exit()
-
-    ## more complicated SEIR+ model
-
-    # initial conditions
-    n_age_groups = 9
-    start_date = (2020, 3, 1, 0, 0, 0)
-    end_date = (2020, 9, 1, 0, 0, 0)
-
-    population = PopulationModel(
-        country='United States of America',
-        cases='USA-Illinois',
-        population_served=12659682,
-        suspected_cases_today=215,
-        ICU_beds=1e10,  # originally 1055
-        hospital_beds=1e10,  # originally 31649
-        imports_per_day=5.0,
-    )
-    age_distribution = AgeDistribution(
-        bin_edges=np.arange(0, 90, 10),
-        counts=[39721484, 42332393, 46094077, 44668271, 40348398, 42120077,
-                38488173, 24082598, 13147180]
-    )
-    severity = SeverityModel(
-        id=np.array([0, 2, 4, 6, 8, 10, 12, 14, 16]),
-        age_group=np.arange(0., 90., 10),
-        isolated=np.array([0., 0., 0., 0., 0., 0., 0., 0., 0.]),
-        confirmed=np.array([5., 5., 10., 15., 20., 25., 30., 40., 50.]),
-        severe=np.array([1., 3., 3., 3., 6., 10., 25., 35., 50.]),
-        critical=np.array([5., 10., 10., 15., 20., 25., 35., 45., 55.]),
-        fatal=np.array([30., 30., 30., 30., 30., 40., 40., 50., 50.]),
-    )
-    epidemiology = EpidemiologyModel(
-        r0=3.7,
-        incubation_time=5,
-        infectious_period=3,
-        length_hospital_stay=4,
-        length_ICU_stay=14,
-        seasonal_forcing=0.2,
-        peak_month=0,
-        overflow_severity=2
-    )
-    containment = ContainmentModel(start_date, end_date, is_in_days=True)
-    containment.add_sharp_event((2020, 3, 20), 0.5)
-
-    ## run Neher-like model simulation
-    simulation = NeherModelSimulation(
-        epidemiology, severity, population.imports_per_day,
-        n_age_groups, containment
-    )
-    y0 = simulation.get_initial_population(population, age_distribution)
-    t0_new = time.time()
-    new_result = simulation([start_date, end_date], y0, lambda x: x, dt=0.001)
-    t1_new = time.time()
-    new_dates = [datetime(2020, 1, 1)+timedelta(x) for x in new_result.t]
-
-    
-    dkeys = ['time', 'susceptible', 'exposed', 'infectious', 'hospitalized',
-             'critical', 'recovered', 'dead']
-
-    print("new method elapsed:", t1_new-t0_new, "s")
-
-    # make figure
-    fig = plt.figure(figsize=(10,8))
-    ax1 = plt.subplot(1,1,1)
-
-
-    for key in dkeys[1:]:
-        ax1.plot(new_dates, new_result.y[key].sum(axis=1), label=key)
-
-    # plot on y log scale
-    ax1.set_yscale('log')
-    ax1.set_ylim(ymin=1)
-
-    # plot x axis as dates
-    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
-    fig.autofmt_xdate()
-
-    # formatting hints
-    ax1.legend()
-    ax1.set_xlabel('time')
-    ax1.set_ylabel('count (persons)')
-
-    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-    plt.savefig('stochastic_examples.png')
