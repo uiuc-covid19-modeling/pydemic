ffc54fa6a9d65568af65ed55c2241db53698ad22
George N. Wong <gnwong2@illinois.edu>
2020-03-30 15:46:41 -0500

    add contribution plotter to statistics testing script

--- statistics/working.py
+++ statistics/working.py
@@ -37,23 +37,29 @@ if __name__ == "__main__":
     icases_zero = np.argmax(cases.deaths>0) + 2
     y_data = cases.deaths[icases_zero:]
 
-    likelihood, (y_data, y_model, y_below, y_above) = neher.calculate_likelihood_for_model(model_params, y_data, n_samples=200)
+    likelihood, (y_data, y_model, y_below, y_above, contr) = neher.calculate_likelihood_for_model(model_params, y_data, n_samples=100)
     print(model_params, likelihood)
     print("====")
 
 
-    ax1 = plt.subplot(2,1,1)
+    ax1 = plt.subplot(3,1,1)
     ax1.plot(y_data, 'xk')
     ax1.plot(y_model)
     ax1.plot(y_above)
     ax1.plot(y_below)
 
-    ax2 = plt.subplot(2,1,2)
+    ax2 = plt.subplot(3,1,2)
     ax2.plot(y_data, 'xk')
     ax2.plot(y_model)
     ax2.plot(y_above)
     ax2.plot(y_below)
     ax2.set_yscale('log')
+    ax2.set_ylim(1.,1.e2)
+
+    ax3 = plt.subplot(3,1,3)
+    ax3.plot(contr, 'ok')
+    #ax3.set_yscale('log')
+    #ax3.set_ylim(1.,1.e2)
 
     plt.savefig('working.png')
 
