2c95addbfaab7b0f490a8f392009fc5a9b414c69
zachjweiner <zachjweiner@gmail.com>
2020-03-31 16:12:01 -0500

    add LikelihoodEstimatorBase and NeherModelEstimator with example

--- pydemic/models/__init__.py
+++ pydemic/models/__init__.py
@@ -23,6 +23,30 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
+import numpy as np
+
+
+def l2_log_norm(a, b):
+    return np.sum(np.power(np.log(a)-np.log(b), 2.))
+
+
+class LikelihoodEstimatorBase:
+    def __init__(self, fit_priors, fixed_parameters, data, norm=None):
+        self.fit_priors = fit_priors
+        self.fixed_parameters = fixed_parameters
+        self.data = data
+
+        if norm is None:
+            self.norm = l2_log_norm
+        else:
+            self.norm = norm
+
+    def __call__(self, theta):
+        raise NotImplementedError
+
+    def get_model_result(self, **kwargs):
+        raise NotImplementedError
+
 
 from pydemic.models.seir import SEIRModelSimulation
 from pydemic.models.neher import NeherModelSimulation
