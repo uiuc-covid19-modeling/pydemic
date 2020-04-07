fa106ba9d8df9edbb59542af501b7afffe468ec2
George N. Wong <gnwong2@illinois.edu>
2020-04-07 18:43:20 -0500

    make code cleaner

--- examples/sample_nonmarkovian.py
+++ examples/sample_nonmarkovian.py
@@ -4,17 +4,23 @@ import numpy as np
 
 pool_size = 4
 walkers = 8
-steps = 200
-discard = 100
-thin = 10
+steps = 500
+discard = 200
+thin = 20
 population = "USA-Illinois"
 age_dist_pop = "United States of America"
 from pydemic.data import united_states
 data = united_states.get_case_data('IL')
 fit_cumulative = False
 
+
 from pydemic.sampling import SampleParameter
 from pydemic import days_to_dates
+from pydemic.models.nonmarkovian import NonMarkovianModelEstimator
+from multiprocessing import Pool
+from common import all_labels, plot_deaths_and_positives
+import corner
+
 
 fit_parameters = [
     SampleParameter('r0', (0.5, 8), 3, .2),
@@ -34,72 +40,12 @@ fixed_values = dict(
     #length_ICU_stay=14,
 )
 
-from pydemic.models.nonmarkovian import NonMarkovianModelEstimator
 estimator = NonMarkovianModelEstimator(
     fit_parameters, fixed_values, data, {'dead': 1, 'positive': 0},
     fit_cumulative=fit_cumulative
 )
 
 
-# for sday in range(40,60): #range(30,45): #[30, 45]:
-#     parameters = {
-#         'r0': 2.8,
-#         'start_day': sday
-#     }
-#     x = estimator.get_log_likelihood(parameters)
-#     print(sday, x)
-
-# best_fit = {
-#     'r0': 2.8,
-#     'start_day': 52
-# }
-
-
-
-
-# best_fit = {
-#     'r0': 2.8,
-#     'start_day': 55.
-# }
-# tt = np.linspace(best_fit['start_day']+1, fixed_values['end_day'], 1000)
-# data = estimator.get_model_data(tt-1, **best_fit, **fixed_values)
-
-# import matplotlib.pyplot as plt
-# fig = plt.figure(figsize=(8,8))
-# ax = plt.subplot(1,1,1)
-# #ax2 = plt.subplot(2,1,2)
-# for key in data.y:
-#     ax.plot(days_to_dates(data.t), data.y[key], label=key)
-
-# fig.autofmt_xdate()
-# #ax.plot()
-# ax.legend()
-# ax.set_ylim(ymin=0.8, ymax=2.e7)
-# ax.set_yscale('log')
-# plt.savefig('imgs/test.png')
-
-# exit()
-# data2 = estimator.get_model_data(tt, **best_fit, **fixed_values)
-
-# import matplotlib.pyplot as plt
-# ax1 = plt.subplot(2,1,1)
-# ax2 = plt.subplot(2,1,2)
-# #ax3 = plt.subplot(3,1,3)
-# ax1.plot(data.t, data.y['infected'])
-# ax2.plot(data.t, data.y['positive'])
-# ax1.set_yscale('log')
-# ax2.set_yscale('log')
-# plt.savefig('imgs/test.png')
-# print(data)
-
-# exit()
-
-# fig = plot_deaths_and_positives(data, best_fit, fixed_values)
-# fig.savefig("imgs/nonmarkovian-best-fit.pdf", bbox_inches='tight')
-
-
-
-from multiprocessing import Pool
 pool = Pool(pool_size)
 sampler = estimator.sample_emcee(
     steps, walkers=walkers, #pool=pool,
@@ -113,9 +59,6 @@ flat_samples = sampler.get_chain(discard=discard, thin=thin, flat=True)
 likelihoods = sampler.get_log_prob(discard=discard, thin=thin, flat=True)
 best_fit = dict(zip(estimator.fit_names, flat_samples[np.argmax(likelihoods)]))
 
-from common import all_labels, plot_deaths_and_positives
-
-import corner
 fig = corner.corner(flat_samples,
                     labels=[all_labels[key] for key in estimator.fit_names],
                     quantiles=[0.16, 0.5, 0.84],
